374dffb06c24d9894c1b5bfdb39c6173b751f020
pooja11-git <157095623+pooja11-git@users.noreply.github.com>
2024-05-28 14:59:44 +0530

    "Support different delimiters Code"

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,24 @@
 class StringCalculator
   def self.add(num_str)
     return 0 if num_str.empty?
-    num_str.split(',').map(&:to_i).sum
+
+    delimiter = ","
+    custom_delimiter_match = num_str.match(/^\/\/(\[.*\]|\W)\n/)
+    if custom_delimiter_match
+      delimiter = parse_custom_delimiters(custom_delimiter_match[1])
+      num_str = num_str.split("\n", 2)[1]
+    end
+
+    num_str.split(Regexp.union(delimiter)).map(&:to_i).sum
+  end
+
+  private
+
+  def self.parse_custom_delimiters(delimiters)
+    if delimiters.start_with?('[') && delimiters.end_with?(']')
+      delimiters[1..-2].split('][')
+    else
+      [delimiters]
+    end
   end
 end
